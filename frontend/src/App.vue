fb3d67f0b9d21c962b7ec4efcc5c877e6f1ed97e
Josh-TX <12686624+Josh-TX@users.noreply.github.com>
2026-09-16 23:45:47 -0500

    moved logout to dropdown

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -28,7 +28,7 @@ onMounted(refreshAuth)
   <template v-if="!authState.checked"></template>
   <LoginView v-else-if="!authState.authenticated" />
   <template v-else>
-    <header class="container-fluid">
+    <header class="container-fluid" style="padding-right: 0;">
       <nav>
         <ul>
           <li><strong>Podtainer</strong></li>
@@ -41,8 +41,15 @@ onMounted(refreshAuth)
           <li :class="{ active: activeSection === 'images' }"><RouterLink to="/images">Images</RouterLink></li>
           <li :class="{ active: activeSection === 'volumes' }"><RouterLink to="/volumes">Volumes</RouterLink></li>
         </ul>
-        <ul>
-          <li><a href="#" @click.prevent="logout">Log out</a></li>
+        <ul style="margin-left: auto; margin-right: 0;">
+          <li style="padding: 0;">
+            <details class="dropdown">
+              <summary aria-label="User menu" style="border: none; padding: 0.5rem 0.75rem;">⋮</summary>
+              <ul dir="rtl">
+                <li><a href="#" @click.prevent="logout">Log out</a></li>
+              </ul>
+            </details>
+          </li>
         </ul>
       </nav>
     </header>
@@ -51,3 +58,20 @@ onMounted(refreshAuth)
     </main>
   </template>
 </template>
+
+<style scoped>
+nav details.dropdown > summary::after {
+  display: none;
+}
+nav details.dropdown > summary {
+  background: none;
+}
+nav details.dropdown > summary:focus,
+nav details.dropdown > summary:focus-visible {
+  box-shadow: none;
+}
+nav details.dropdown > summary:hover,
+nav details.dropdown[open] > summary {
+  background-color: var(--pico-dropdown-hover-background-color);
+}
+</style>
